bcbcf964045f19d405447af6459bcb93d8db79da
Toby Fox <tobias.fox@dowjones.com>
2013-12-26 18:44:16 -0500

    Making site totally static, compiling jade in app.js

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var jade = require('jade');
+var fs = require('fs');
 
 var app = express();
 
@@ -27,8 +28,61 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+// this site is entirely static. we just have to use jade to generate public/index.html
+// TODO i think that right now the less file is only compiled to CSS when the server gets hit for stylesheets/style.css. maybe disable less-middleware and just compile it right now
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// clunky way to check whether we're online or not
+require('dns').resolve('www.google.com', function(err) {
+
+  if (err) { // not connected
+    console.log("Not connected to Internet, will use local JS files");
+    var scripts = [
+      "/js/jquery.min.js",
+      //"/js/jquery-ui.min.js",
+      "/js/underscore.min.js",
+      "/js/angular.min.js",
+      //"/js/angular-animate.min.js",
+      "/js/firebase.js",
+      //"/js/angularfire.min.js",
+      "/js/firebase-simple-login.js"
+    ];
+  }
+  else { // connected
+    console.log("Resolved google.com so think we're connected, will use CDNs for JS");
+    var scripts = [
+      "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
+      //"//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
+      "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js",
+      "//ajax.googleapis.com/ajax/libs/angularjs/1.2.5/angular.min.js",
+      //"//ajax.googleapis.com/ajax/libs/angularjs/1.2.5/angular-animate.min.js",
+      "//cdn.firebase.com/v0/firebase.js",
+      //"//cdn.firebase.com/libs/angularfire/0.3.0/angularfire.min.js",
+      "https://cdn.firebase.com/v0/firebase-simple-login.js"
+    ];
+  }
+
+  var view = {
+    'scripts': scripts.concat([
+      // scripts served by this server:
+      "/js/lunr.min.js",
+      "/js/js.js"
+    ]),
+    styles: [
+      '/stylesheets/style.css',
+      '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'
+    ]
+  };
+
+  console.log("Compiling index.html from jade...");
+  jade.renderFile('views/index.jade', view, function (err, html) {
+    if (err) throw err;
+
+    fs.writeFile("public/index.html", html, function(err) {
+      if (err) throw err;
+
+      http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+      });
+    }) // end fs writing index.html
+  }); // end jade rendering
+}); // end dns attempt to resolve google
