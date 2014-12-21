113d13741f482f361503f0cfe4afba22a2ae8837
Toby Fox <tobyfox@gmail.com>
2014-12-21 01:21:37 -0500

    Firebase backup tool

---
+++ backup.js
@@ -0,0 +1,35 @@
+/**
+ * outputs complete JSON content of Firebase to stdout
+ * all other output is to stderr
+ *
+ * example invocation (e.g. in cron job):
+ *
+ *     filename=/home/ubuntu/nutmeg/backups/firebase_dump_`date +"%Y-%m-%d"`.json && node /home/ubuntu/nutmeg/backup.js > $filename && gzip $filename
+ */
+
+var config = require('./config.json');
+var Firebase = require('firebase');
+var FirebaseTokenGenerator = require('firebase-token-generator');
+
+var tokenGenerator = new FirebaseTokenGenerator(config.FIREBASE_SECRET);
+var token = tokenGenerator.createToken({uid: 'toby-admin'}, {admin: true});
+
+var ref = new Firebase('https://nutmeg.firebaseio.com/');
+ref.authWithCustomToken(token, function(error, authData) {
+  if (error) {
+    console.error('Login Failed!', error);
+    process.exit(1);
+  } else {
+    console.error('Authenticated successfully with payload:', authData);
+  }
+});
+
+ref.once('value', function(data) {
+  console.error('successfully retrieved data');
+  var everything = data.val();
+  console.log(JSON.stringify(everything));
+  process.exit();
+}, function(err) {
+  console.error('failed to get data:', err);
+  process.exit(1);
+});
