4de91019c02148f47b81703154816e423541f517
Toby Fox <toby@digg.com>
2016-04-02 19:21:57 -0400

    More LoginComponent stubbing + pseudo login shit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,10 +16,28 @@ export class LoginComponent {
   constructor(private accountService: AccountService) {
   }
 
-  ngOnInit() {
-  }
-
   login() {
     this.accountService.login(this.email, this.password);
+  }
+
+  /** Shenanigans needed to get Chrome to offer to remember password. Copies credentials into pseudo-login.html inside iframe and submit it, since Chrome requires actual form submission and page load before it offers to remember. pseudo-login.html is also used as form `action` - `about:blank` and cancelling submission didn't work. */
+  pseudoLogin (loginIframe: HTMLIFrameElement) {
+    var loginIframeDoc = loginIframe.contentWindow ? loginIframe.contentWindow.document : loginIframe.contentDocument;
+    
+    var emailInput: HTMLInputElement = <HTMLInputElement>loginIframeDoc.getElementById('email');
+    var passwordInput: HTMLInputElement = <HTMLInputElement>loginIframeDoc.getElementById('password');
+    var loginForm: HTMLFormElement = <HTMLFormElement>loginIframeDoc.getElementById('login-form');
+
+    emailInput.value = this.email;
+    passwordInput.value = this.password;
+    loginForm.submit();
+  }
+
+  forgotPassword() {
+
+  }
+
+  createAccount() {
+    
   }
 }
