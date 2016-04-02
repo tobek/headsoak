f8f8bc0499628270c8bef0072c4e16c5ff20f455
Toby Fox <toby@digg.com>
2016-04-02 13:48:44 -0400

    Fix tests and misc config bugs

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -7,7 +7,7 @@ describe('App', () => {
 
   it('should have a title', () => {
     let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2990 from @AngularClass';
+    let result  = 'nutmeg';
     expect(subject).toEqual(result);
   });
 
--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -8,15 +8,15 @@ describe('App', () => {
 
   it('should have a title', () => {
     let subject = browser.getTitle();
-    let result  = 'Angular2 Webpack Starter by @gdi2990 from @AngularClass';
+    let result  = 'nutmeg';
     expect(subject).toEqual(result);
   });
 
-  it('should have `your content here` x-large', () => {
-    let subject = element(by.css('[x-large]')).getText();
-    let result  = 'Your Content Here';
-    expect(subject).toEqual(result);
-  });
+  // it('should have `your content here` x-large', () => {
+  //   let subject = element(by.css('[x-large]')).getText();
+  //   let result  = 'Your Content Here';
+  //   expect(subject).toEqual(result);
+  // });
 
 
 });
