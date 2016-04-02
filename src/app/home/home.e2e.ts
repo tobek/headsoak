3fa4d05eadc10fd8083c3e4a83b8fb1b03e8cc6d
Toby Fox <toby@digg.com>
2016-04-02 14:58:51 -0400

    Fix Home e2e test

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -2,7 +2,7 @@ describe('App', () => {
 
   beforeEach(() => {
     // change hash depending on router LocationStrategy
-    browser.get('/#/home');
+    browser.get('/');
   });
 
 
@@ -12,11 +12,12 @@ describe('App', () => {
     expect(subject).toEqual(result);
   });
 
-  // it('should have `your content here` x-large', () => {
-  //   let subject = element(by.css('[x-large]')).getText();
-  //   let result  = 'Your Content Here';
-  //   expect(subject).toEqual(result);
-  // });
+  it('should have `HMR data test` x-large', () => {
+    debugger;
+    let subject = element(by.css('[x-large]')).getText();
+    let result  = 'HMR data test';
+    expect(subject).toEqual(result);
+  });
 
 
 });
