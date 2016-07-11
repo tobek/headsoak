9e049680d344aaf3f2ec0ad6250e9d5bae86863a
Toby Fox <toby@digg.com>
2016-07-10 22:10:33 -0400

    Tags Service: Stub unit test

---
+++ src/app/tags/tags.service.spec.ts
@@ -0,0 +1,35 @@
+import {
+  it,
+  inject,
+  injectAsync,
+  beforeEach,
+  beforeEachProviders,
+} from 'angular2/testing';
+
+import {TagsService} from './';
+
+var SAMPLE_TAGS = [
+  { name: 'Cool tag bro', id: '1' },
+  { name: 'Cooler tag, bro', id: '2' },
+];
+
+describe('TagsService', () => {
+  beforeEachProviders(() => [
+    TagsService,
+  ]);
+
+  var tagsService;
+  beforeEach(inject([ TagsService ], (injectedService) => {
+    tagsService = injectedService;
+  }));
+
+  it('should initialize tags', () => {
+    tagsService.init(SAMPLE_TAGS);
+
+    expect(_.keys(tagsService.tags).length).toBe(2);
+  });
+
+  // @TODO/tests
+  // lots more
+
+});
