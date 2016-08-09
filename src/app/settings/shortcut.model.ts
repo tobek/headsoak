f5472419bb968bdac4fb2bbd176ccf98dd5d785f
Toby Fox <tobyfox@gmail.com>
2016-08-09 15:29:14 -0400

    Add Mousetrap library

--- src/app/settings/shortcut.model.ts
+++ src/app/settings/shortcut.model.ts
@@ -4,7 +4,11 @@ import {Setting} from './';
 
 export class Shortcut extends Setting {
   static VALID_MOD_KEYS = ['ctrl', 'shift', 'alt', 'option', 'meta', 'mod', 'command'];
-  
+
+  /** Shortcut binding. Will be combined with global modifier key setting `sMod` (unless `noMod`) and passed directly to Mousetrap - see Mousetrap docs for more info. */
+  value: string;
+
+  /** The function that gets run when this shortcut is used. */
   fn: Function;
 
   /** Work even in text input areas without "mousetrap" class. */
