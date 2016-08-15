b8c1c8773c25b6c4bd6ce5a9a4aa2e367fe55051
Toby Fox <tobyfox@gmail.com>
2016-08-15 12:12:14 -0400

    Active UIs support for observables to fire when UI becomes active

--- src/app/active-uis.service.ts
+++ src/app/active-uis.service.ts
@@ -1,21 +1,63 @@
 import {Injectable} from '@angular/core';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
 
 import {HomeComponent} from './home';
 import {Note, NoteBrowserComponent, NoteComponent} from './notes';
 
 @Injectable()
 export class ActiveUIsService {
-  public home: HomeComponent;
-  
-  public noteBrowser: NoteBrowserComponent;
+  private _home: HomeComponent;
+
+  private _noteBrowser: NoteBrowserComponent;
 
   /** Note where the textarea currently has focus. */
-  public focusedNoteComponent: NoteComponent;
+  private _focusedNoteComponent: NoteComponent;
 
   /** Note which is open in the main writing area. May be the same as focusedNoteComponent. */
-  public openNoteComponent: NoteComponent;
+  private _openNoteComponent: NoteComponent;
+
+  private _home$ = new ReplaySubject<HomeComponent>(1);
+  private _noteBrowser$ = new ReplaySubject<NoteBrowserComponent>(1);
+  private _focusedNoteComponent$ = new ReplaySubject<NoteComponent>(1);
+  private _openNoteComponent$ = new ReplaySubject<NoteComponent>(1);
+
+  // The following can be subscribed to to be notified of updates to the active UI. If there is a current truthy value, any subscription will immediately be called with that value. Subscribing with `.first().subscribe(...)` will call immediately with current truthy value if any and then unsubscribe, otherwise will be called when there is a truthy value and then unsubscribe. (Filtering the private subject is what ensures that if the active UI is set to null, it doesn't replay or emit a null value.)
+  public home$ = this._home$.filter(val => !! val);
+  public noteBrowser$ = this._noteBrowser$.filter(val => !! val);
+  public focusedNoteComponent$ = this._focusedNoteComponent$.filter(val => !! val);
+  public openNoteComponent$ = this._openNoteComponent$.filter(val => !! val);
 
   constructor(
   ) {
+  }
+
+  get home(): HomeComponent {
+    return this._home;
+  }
+  get noteBrowser(): NoteBrowserComponent {
+    return this._noteBrowser;
+  }
+  get focusedNoteComponent(): NoteComponent{
+    return this._focusedNoteComponent;
+  }
+  get openNoteComponent(): NoteComponent {
+    return this._openNoteComponent;
+  }
+
+  set home(newVal: HomeComponent) {
+    this._home = newVal;
+    this._home$.next(newVal);
+  }
+  set noteBrowser(newVal: NoteBrowserComponent) {
+    this._noteBrowser = newVal;
+    this._noteBrowser$.next(newVal);
+  }
+  set focusedNoteComponent(newVal: NoteComponent){
+    this._focusedNoteComponent = newVal;
+    this._focusedNoteComponent$.next(newVal);
+  }
+  set openNoteComponent(newVal: NoteComponent) {
+    this._openNoteComponent = newVal;
+    this._openNoteComponent$.next(newVal);
   }
 }
