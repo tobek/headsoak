f38ebdb0391c3a5dd38d06993e6cb07ea214dea2
Toby Fox <tobyfox@gmail.com>
2016-09-18 23:26:13 -0400

    Account Settings: Include Settings component

--- src/app/account/account-settings.component.ts
+++ src/app/account/account-settings.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {SettingsComponent} from '../settings';
 
 @Component({
   selector: 'account-settings',
-  styles: [`
-  `],
+  directives: [
+    SettingsComponent
+  ],
   template: `
     <h1>
       Account Settings
     </h1>
+
+    <settings></settings>
+
     <!-- @TODO/rewrite This doesn't work obvs, but we can pass the message on. -->
     <a (click)="loginComponent.deleteAccount()">Delete account</a>
   `
