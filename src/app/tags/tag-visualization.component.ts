8cb80f1641f2e233fe5165e5ab32bdae5f5f9dca
Toby Fox <tobyfox@gmail.com>
2016-10-09 15:30:32 -0400

    Tags: Viz: Separate subtags

--- src/app/tags/tag-visualization.component.ts
+++ src/app/tags/tag-visualization.component.ts
@@ -13,7 +13,13 @@ const d3 = require('d3');
 type TagLink = {
   source: string,
   target: string,
-  weight: number
+  weight: number,
+};
+type TagNode = {
+  id: string,
+  name: string,
+  docs: string[],
+  prog?: boolean,
 };
 
 @Component({
@@ -48,10 +54,32 @@ export class TagVisualizationComponent {
 
   ngAfterViewInit() {
     this.initGraph({
-      nodes: _.values(this.tagsService.tags),
+      // nodes: _.values(this.tagsService.tags),
+      nodes: this.computeNodes(),
       links: this.computeLinks(),
     });
     // this.initGraph(graph3);
+  }
+
+  computeNodes(): TagNode[] {
+    // We use _.flatten because while mapping we sometimes return an array of subtags
+    return _.flatten(_.map(this.tagsService.tags, (tag: Tag) => {
+      if (_.size(tag.subTagDocs)) {
+        // Return an array of subtags (molded to fit TagNode type)
+        return _.map(tag.subTagDocs, (docs, subTagName): TagNode => {
+          return {
+            id: tag.id + ':' + subTagName,
+            name: tag.name + ': ' + subTagName,
+            docs: docs,
+            prog: tag.prog,
+          };
+        });
+      }
+      else {
+        // Just return the tag!
+        return tag;
+      }
+    }));
   }
 
   computeLinks(): TagLink[] {
@@ -60,15 +88,17 @@ export class TagVisualizationComponent {
     this.isCrowded = _.size(this.tagsService.tags) > 100;
 
     const tagLinkIndex = {};
-    let tagLink;
+    const separator = '👻🌚🌀🌱'; // need something unlikely to be in a subtag name
 
     _.each(this.tagsService.dataService.notes.notes, (note) => {
       if (! note.tags || note.tags.length < 2) {
         return;
       }
 
-      // Filter out shared tags or broken stuff
-      const validTags = note.tags.filter(tagId => this.tagsService.tags[tagId]);
+      // Get actual Tag instances and filter out shared tags or broken stuff
+      const validTags = note.tags
+        .map(tagId => this.tagsService.tags[tagId])
+        .filter(tag => tag);
 
       if (validTags.length < 2) {
         return;
@@ -77,7 +107,30 @@ export class TagVisualizationComponent {
       // We want to get every pairing of tags on this note, but since links are bidirectional we don't need the reverse, e.g. tag 4 -> tag 8 and tag 8 -> tag 4.
       for (let i = validTags.length - 1; i >= 1; i--) {
         for (let j = i - 1; j >= 0; j--) {
-          tagLink = validTags[i] + ',' + validTags[j];
+          const sourceTag = validTags[i];
+          const targetTag = validTags[j];
+
+          let sourceTagId = sourceTag.id;
+          let targetTagId = targetTag.id;
+
+          if (_.size(sourceTag.subTagDocs)) {
+            _.each(sourceTag.subTagDocs, (docs, sourceTagName) => {
+              if (docs.indexOf(note.id) !== -1) {
+                sourceTagId += ':' + sourceTagName;
+                return false;
+              }
+            });
+          }
+          if (_.size(targetTag.subTagDocs)) {
+            _.each(targetTag.subTagDocs, (docs, targetTagName) => {
+              if (docs.indexOf(note.id) !== -1) {
+                targetTagId += ':' + targetTagName;
+                return false;
+              }
+            });
+          }
+
+          const tagLink = sourceTagId + separator + targetTagId;
 
           if (! tagLinkIndex[tagLink]) {
             tagLinkIndex[tagLink] = 1;
@@ -87,7 +140,7 @@ export class TagVisualizationComponent {
           }
 
           // Currently we only used this if crowded, so waste to compute otherwise
-          // now note used at all
+          // (now not used at all)
           // if (this.isCrowded) {
           //   if (! this.pairCount[validTags[i].id]) {
           //     this.pairCount[validTags[i].id] = 1;
@@ -107,7 +160,7 @@ export class TagVisualizationComponent {
     });
 
     this.tagLinks = _.map(tagLinkIndex, (weight, link): TagLink => {
-      const links = link.split(',');
+      const links = link.split(separator);
       return {
         source: links[0],
         target: links[1],
