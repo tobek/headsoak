0cd8353bc04aa6acb295c932751959a32b5975de
Toby Fox <tobyfox@gmail.com>
2017-02-08 10:15:47 -0500

    Fix breakage on falsey tag on a note

--- src/app/pipes/internal-tags-filter.pipe.ts
+++ src/app/pipes/internal-tags-filter.pipe.ts
@@ -1,11 +1,14 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
+import {Logger} from '../utils/';
+
 import {Tag} from '../tags/';
 
 import * as _ from 'lodash';
 
 @Pipe({ name: 'internalTagsFilter' })
 export class InternalTagsFilterPipe implements PipeTransform {
+  private _logger: Logger = new Logger(this.constructor.name);
 
   transform(arr: Array<string | Tag>): Array<string | Tag> {
     if (! _.size(arr)) {
@@ -18,7 +21,14 @@ export class InternalTagsFilterPipe implements PipeTransform {
     }
     else {
       // these are Tag instances
-      return _.filter(arr, (tag: Tag) => ! tag.internal);
+      // return _.filter(arr, (tag: Tag) => ! tag.internal);
+      return _.filter(arr, (tag: Tag) => {
+        if (! tag) {
+          this._logger.warn('Falsey tag passed in to internal tags filter!', arr, tag);
+          return false;
+        }
+        return ! tag.internal;
+      });
     }
   }
 }
